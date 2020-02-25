f4cb93310eda957fd829b9a8e054381dd2807cde
김정민 <dsaqaz421@naver.com>
2020-02-25 21:20:47 +0900

    Initial Commit

---
+++ VCStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VCStudy
+//
+//  Created by 김정민 on 2020/02/25.
+//  Copyright © 2020 kjm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
