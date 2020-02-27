e903feef4e52d5f4d00091087c1441ea97b7b3d3
김정민 <dsaqaz421@naver.com>
2020-02-27 19:42:35 +0900

    Navigation Item and Bar 공부

--- VCStudy/NavigationController/FirstViewController.swift
+++ VCStudy/NavigationController/FirstViewController.swift
@@ -10,8 +10,44 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    /*
+        네비게이션 바와 아이템 기본
+     1. 스토리보드에 기본 기능을 확인하기 위해 버튼과 스위치 추가
+     2. 스위치의 액션에 따라 버튼 처리를 위한 action 연결
+     3. 연결된 뷰의 back 버튼을 바꾸려면 루트뷰에서 바꿔줘야함
+     */
+    
+    // (2). sender는 switch로 변경
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        // 스위치에 따라 버튼 숨김 처리
+        switch sender.isOn{
+        case true:
+            // 왼쪽 버튼 숨김 처리
+            navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+            
+            // 오른쪽에 새로운 버튼 추가
+            if var list = navigationItem.rightBarButtonItems{
+                let btn = UIBarButtonItem(title: "Item", style: .plain, target: nil, action: nil)
+                // 오른쪽은 왼쪽과 반대로 배열에 추가된 순서대로 오른쪽에 추가됨
+                // 따라서 배열의 마지막 부분에 버튼을 추가해야함
+                list.append(btn)
+                navigationItem.rightBarButtonItems = list
+            }
+        case false:
+            // 버튼 제거
+            navigationItem.leftBarButtonItem = nil
+            
+            // 오른쪽은 스위치를 제외한 나머지를 제거
+            let list = navigationItem.rightBarButtonItems?.dropLast()
+            navigationItem.rightBarButtonItems = Array(list!)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // back 버튼 명칭 변경 (3)
+        navigationItem.backBarButtonItem?.title = "Go Back"
     }
     
     // Root View로 이동하도록 unwind 선언
--- VCStudy/NavigationController/SecondViewController.swift
+++ VCStudy/NavigationController/SecondViewController.swift
@@ -16,8 +16,22 @@ class SecondViewController: UIViewController {
      2. 코드를 통해 back 기능을 구현할 수 있음
      */
     
+    /*
+       네비게이션 바와 아이템 기본
+     1. back 버튼이 있는 상태에서 아이템을 추가하면 백버튼이 사라지고 새로 추가한 버튼으로 대체됨
+     --> 이 경우에는 이전으로 돌아가는 기능을 코드로 구현해야함
+     --> 함께 표시하고 싶으면 Left items Supplement를 체크해야함(Attribute)
+     2. 코드로 네비게이션 아이템 추가
+     --> 메서드 구현 후 viewDidLoad에서 호출해야함
+    */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 코드로 back 버튼과 버튼이 같이 표시되도록 설정 (2-1)
+        navigationItem.leftItemsSupplementBackButton = true
+        // 새로운 버튼 생성 (2-2)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addRightButtons))
+        
     }
     
     // 두번째 뷰로 이동하도록 unwind 선언
@@ -34,6 +48,19 @@ class SecondViewController: UIViewController {
     // 코드를 통해 back 기능 구현(2)
     @IBAction func pop(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    // 오른쪽에 버튼 추가하는 메서드(2-2)
+    @objc func addRightButtons(){
+        let btn1 = UIBarButtonItem(barButtonSystemItem: .action, target: nil, action: nil)
+        let btn2 = UIBarButtonItem(title: "Two", style: .plain, target: nil, action: nil)
+        
+        // switch는 직접 추가를 못하기 때문에 embed해서 추가해야함
+        let sw = UISwitch()
+        let switchItem = UIBarButtonItem(customView: sw)
+        
+        // 애니메이션 효과를 줘서 네비게이션에 추가
+        navigationItem.setRightBarButtonItems([switchItem, btn1, btn2], animated: true)
     }
     
 }
